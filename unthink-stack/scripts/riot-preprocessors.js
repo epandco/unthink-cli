494add571f206f8668ab5e57246d661f4ccd7d7b
Dan Alexander <dan.alexander@epandcompany.com>
2020-07-31 14:26:21 -0400

    dependency updates and new preprocessor library

--- unthink-stack/scripts/riot-preprocessors.js
+++ unthink-stack/scripts/riot-preprocessors.js
@@ -1,38 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const {registerPreprocessor} = require('@riotjs/compiler');
-const transform = require('./riot-typescript-transformer');
-const {basename, dirname, join} = require('path');
-const {CLIEngine} = require('eslint');
+const {join} = require('path');
 const stripIndent = require('strip-indent');
-const {readFileSync} = require('fs');
 const sass = require('sass');
 const stylelint = require('stylelint');
-
-const eslintRules = JSON.parse(readFileSync(join(process.cwd(), '.eslintrc')));
-const cli = new CLIEngine(eslintRules);
-const formatter = cli.getFormatter();
+const { initRiotTypeScriptPreprocessor } = require('@epandco/riot-typescript-preprocessor');
 
 const CLIENT_ROOT_PATH = join(process.cwd(), 'src', 'client');
 
-// Riot preprocessor for handling TypeScript inside Riot tags.
-// https://riot.js.org/compiler/#pre-processors
-registerPreprocessor('javascript', 'ts', (source, {options}) => {
-  const filename = `${basename(options.file)}.ts`;
-  const fileRoot = dirname(options.file);
-  const lintReport = cli.executeOnText(stripIndent(source), options.file);
+console.log(process.cwd());
 
-  if (lintReport.errorCount > 0 || lintReport.warningCount > 0) {
-    console.log(formatter(lintReport.results)); // eslint-disable-line
-    throw new Error(`Linting reports ${lintReport.errorCount} errors and ${lintReport.warningCount} warnings in Riot components.`);
-  }
-
-  // basic type checking and transformation
-  const {diagnostics, code, map} = transform(filename, source, fileRoot);
-  if (diagnostics && diagnostics.length > 0) {
-    throw new Error(`TypeScript compiler reports ${diagnostics.length} errors in Riot Components.`);
-  }
-  return {code, map};
+initRiotTypeScriptPreprocessor(registerPreprocessor, {
+  sourcePath: CLIENT_ROOT_PATH
 });
 
 // Riot preprocessor for handling Sass inside Riot tags.
